36b63251be327db6c789c708b09f123b499eb01d
gajamvarsha27 <23r11a0518@gcet.edu.in>
2025-05-18 10:53:40 +0530

    update

--- a15/index.js
+++ a15/index.js
@@ -1,7 +1,7 @@
 const products=[
-    {id :1, name:"Product 1",price:34, status:"pending"},
-    {id :2, name:"Product 2",price:24,  status:"pending"},
-    {id :3, name:"Product 3",price:44,  status:"pending"},
+    {id :1, name:"Product 1",price:54, status:"pending"},
+    {id :2, name:"Product 2",price:34,  status:"pending"},
+    {id :3, name:"Product 3",price:48,  status:"pending"},
 ];
 
 const newPrice=products.map((value) => ({ ...value,price:value.price+5,status:value.status="Completed"}));
